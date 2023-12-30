18cf30d0d6a9850d81148efd087f4eadd446b895
Tchaikovsky <84560196+tchaikovsky1984@users.noreply.github.com>
2023-12-30 23:00:54 +0530

    Update att.c - Cleaned up input prompts

--- att.c
+++ att.c
@@ -6,25 +6,25 @@ int main()
 	int i,n=1,pre,occ,n_skip,j,f=1;
 	double att,req;
 	printf("========ATTENDANCE CHECK | BUNK MANAGER========\n");
-	printf("Enter the percentage required in numbers : \t");
+	printf("Percentage required in numbers : \t");
 	scanf("%lf", &req);
-	printf("Enter hours in week : \n");
+	printf("Schedule : \n");
 	for(i=0;i<=5;i++)
 	{
 		printf("\t Day %d ; Hours : \t",i);
 		scanf("%d",&arr[i]);
 	}
-	printf("Enter your present hours (after today's hours): \t");
+	printf("Present Hours: \t");
 	scanf("%d",&pre);
-	printf("Enter total hours (after today's hours): \t");
+	print("Total Hours: \t");
 	scanf("%d",&occ);
-	printf("Enter day tomorrow : \t");
+	printf("Next working day: \t");
 	scanf("%d",&i);
 	att = (double)pre/(double)occ;
-	printf("Enter number of days you are skipping : \t");
+	printf("Number of absences: \t");
 	scanf("%d",&n_skip);
 	if(n_skip != 0)
-		printf("Enter the days you are skipping (Next working day is day 1, so if you want to skip the day after tomorrow enter 2 and so on. And if you are skipping multiple days enter all the day numbers. Enter after each day.Enter in ascneding order.): \n");
+		printf("Skipping the following days:\n");
 	skip = (int*)malloc(sizeof(int)*n_skip);
 	for(j=0;j<n_skip;j++){
 		scanf("%d",&skip[j]);
@@ -49,7 +49,7 @@ int main()
 		{
 			occ = occ + arr[i];
 			att = (double)pre/(double)occ;
-			printf("Day %d : %d / %d - %lf\n",n,pre,occ,(att*100));
+			printf("%d\t%d/%d\t%lf\t(SKIPPED)\n",n,pre,occ,(att*100));
 			i = (i+1)%6;
 			n++;
 			j++;
